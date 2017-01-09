a2bc645ece1e9e6aa5626ead94f964c371132bd2
Lucas Monteverde <monteverde13@gmail.com>
2017-01-09 20:06:31 -0200

    new tabs scripts cleanup

--- js/tabs.js
+++ js/tabs.js
@@ -1,24 +1,29 @@
-function getUrls(s) {
-	return s.match(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi);
-}
+const URLS = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
 chrome.contextMenus.removeAll();
 
-chrome.contextMenus.create({"title" : "Open links in new tabs",	"contexts" : ["selection"],	"onclick": function(info, tab) {
-	console.log(info);
-	
-	var selected_text = info.selectionText;
-	
-	if(selected_text && selected_text != null){
-	
-		var text_links = getUrls(selected_text)
-		console.log(text_links);
+chrome.contextMenus.create({
+	title : 'Open links in new tabs', 
+	contexts : ['selection'], 
+	onclick: function(info) {
+
+		const selectedText = info.selectionText;
 		
-		chrome.tabs.getSelected(null,function(tab) {
-			for(var i = 0; i< text_links.length; i++){
-				//console.log(text_links[i]);
-				chrome.tabs.create({url: text_links[i],active: false, index: tab.index + 1 + i, openerTabId: tab.id});
-			}
-		})
+		if( selectedText ) {
+		
+			const links = selectedText.match(URLS);
+
+			links.forEach( link => window.open(link, '_blank') );
+			
+			/*chrome.tabs.getSelected(null,function(tab) {
+				for(var i = 0; i< text_links.length; i++){
+					chrome.tabs.create({
+						url: text_links[i],active: false, 
+						index: tab.index + 1 + i, 
+						openerTabId: tab.id
+					});
+				}
+			})*/
+		}
 	}
-}});
+});
